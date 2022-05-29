2bb8f75fb0d8363261ccc73fb5acdff4dd10a871
Sanjeev Tiwari <stcloudgate@gmail.com>
2022-05-29 15:15:48 +0530

    fix for api call
    Signed-off-by: Sanjeev Tiwari <stcloudgate@gmail.com>

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -12,7 +12,7 @@ export class PostsService {
   constructor(private http: HttpClient) {}
 
   getPosts() {
-    this.http.get<{message: string, posts: Post[]}>('http://localhost:3000/api/posts')
+    this.http.get<{message: string, posts: Post[]}>('http://ng-svc-backend:3001/api/posts')
       .subscribe((postData) => {
         this.posts = postData.posts;
         this.postsUpdated.next([...this.posts]);
@@ -25,7 +25,7 @@ export class PostsService {
 
   addPost(title: string, content: string) {
     const post: Post = { id: null, title: title, content: content };
-    this.http.post<{message: string}>('http://localhost:3000/api/posts', post)
+    this.http.post<{message: string}>('http://ng-svc-backend:3001/api/posts', post)
     .subscribe((responseData) => {
         console.log(responseData.message);
         this.posts.push(post);
